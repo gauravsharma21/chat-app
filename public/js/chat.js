ee9a6869113f24f128947a9bab4915ef76b1c9b6
Gaurav Sharma <gauravsharma10167@gmail.com>
2019-07-28 10:48:54 +0530

    modified autoscrolling function

--- public/js/chat.js
+++ public/js/chat.js
@@ -12,20 +12,8 @@ const sidebarTemplate = document.querySelector('#sidebar-template').innerHTML
 
 const { username, room } = Qs.parse(location.search, {ignoreQueryPrefix : true})
 const autoscroll = () => {
-    const $newMessage = $messages.lastElementChild
-    const newMessageStyles = getComputedStyle($newMessage)
-    const newMessageMargin = parseInt(newMessageStyles.marginBottom)
-    const newMessageHeight = $newMessage.offsetHeight + newMessageMargin
-
-    const visibleHeight = $messages.offsetHeight
-
-    const containerHeight = $messages.scrollHeight
-
-    const scrollOffset = $messages.scrollTop + visibleHeight
-
-    if (containerHeight - newMessageHeight <= scrollOffset) {
-        $messages.scrollTop = $messages.scrollHeight
-    }
+    const element=$messages.lastElementChild
+    element.scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"})
 }
 
 socket.on('message', (message) => {
